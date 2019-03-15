0dd65fd8123bbc47c41827df6a62fb3ab2a8aa28
keithchasen <keithchasen89@gmail.com>
2019-03-15 16:31:33 +0200

    basic posts looping inside the home template

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Keith Chasen',
+        'title': 'Blog Post 1',
+        'content': 'This is a brand new post',
+        'date_posted': 'March 15, 2019'
+    },
+    {
+        'author': 'Jack Doe',
+        'title': 'Blog Post 2',
+        'content': 'This is another post',
+        'date_posted': 'March 15, 2019'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
