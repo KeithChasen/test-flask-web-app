0c140b2bca2705a3b34605db8e9ba1b3698d6f7c
keithchasen <keithchasen89@gmail.com>
2019-03-20 09:49:37 +0200

    basic registration

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '27526d42886bf80c026c9e2838b62f7a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
